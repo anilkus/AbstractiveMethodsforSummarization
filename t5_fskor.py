3211829bdca2b24faac0e0d35a5bbafd5402f582
Anıl Kuş <anilkus01@gmail.com>
2023-09-07 14:58:44 +0300

    Update t5_fskor.py

--- t5_fskor.py
+++ t5_fskor.py
@@ -1,17 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 14 15:11:56 2023
 
-@author: LENOVO
-"""
-
-# Dosyanın içeriğini oku
-with open("C:/Users/LENOVO/Desktop/TEZ/Veri Ön İşleme/Abstracts/z_Uludağ Üniversitesi Tıp Fakültesi Dergisi/971366-1880431.txt", 'r',encoding="utf-8") as text2:
+with open("Original_Text.txt", 'r',encoding="utf-8") as text2:
     abstract=text2.read()
-with open("C:/Users/LENOVO/Desktop/TEZ/Veri Ön İşleme/Çalışmanın Özetleri/GPT/z_Uludağ Üniversitesi Tıp Fakültesi Dergisi/971366-1880431.txt", 'r',encoding="utf-8") as text:
-    islenecek=text.read()
-    summary=islenecek
-
+with open("Target_Text.txt", 'r',encoding="utf-8") as text:
+    summary=text.read()
+    
 from rouge import Rouge
 ROUGE = Rouge()
 
